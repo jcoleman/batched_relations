67ad206fcf06c7e29f299f58061292b4aed6603c
jcoleman <jtc331@gmail.com>
2018-10-12 13:47:35 -0400

    Remove references to as-yet untracked files.

--- lib/batched_relations.rb
+++ lib/batched_relations.rb
@@ -3,5 +3,3 @@ end
 
 require "batched_relations/version"
 require "batched_relations/batch"
-require "batched_relations/active_record_base_extension"
-require "batched_relations/active_record_relation_extension"
